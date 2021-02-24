aade0498fdd2638fdd21ef199010eceb4ab10e63
Michael Schiebein <moomba0@gmail.com>
2021-02-24 20:15:13 +0300

    remove auth

--- keymix/auth.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-client_id = 'a598514bd4fa4304b7ca63c5046fca6a'
-client_secret = 'ce25bcaa33434f6cae6e39d44ef48446'
-redirect_uri_encoded = 'https%3A%2F%2Fkeymix.herokuapp.com%2Fuser_auth'
-redirect_uri = "https://keymix.herokuapp.com/user_auth"
-secret_key = "4$2#%%3se@#$cret"
-encodedData = base64.b64encode(
-    bytes(f"{client_id}:{client_secret}", "ISO-8859-1")).decode("ascii")
