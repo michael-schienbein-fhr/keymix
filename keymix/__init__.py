39909babb5b45e4010867fd32c9fbc4fd0497871
Michael Schiebein <moomba0@gmail.com>
2021-02-24 19:24:23 +0300

    changed entire app structure

---
+++ keymix/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# from flask_debugtoolbar import DebugToolbarExtension
+# from auth import secret_key
+
+
+app = Flask(__name__)
+# testing:
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///keymix'
+app.config['SECRET_KEY'] = 'secret_key'
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# debug = DebugToolbarExtension(app)
+# production:
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+#     'DATABASE_URL', 'postgres:///flask-heroku')
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'akey')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy()
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+connect_db(app)
+db.create_all()
+
+from keymix import routes, forms, models, auth, util
---
+++ keymix/util.py
@@ -0,0 +1,75 @@
+import requests
+
+def get_user_id(token):
+    """Returns the logged in user's Spotify ID"""
+
+    headers = {'Authorization': 'Bearer ' + token}
+    resp = requests.get('https://api.spotify.com/v1/me',
+                        headers=headers).json()
+    return resp['id']
+
+
+def get_id(input_name, input_type, token):
+    """Returns a response with artists or tracks id"""
+
+    headers = {'Authorization': 'Bearer ' + token}
+    payload = {
+        'q': input_name,
+        'type': input_type,
+        'limit': 10
+    }
+    resp = requests.get('https://api.spotify.com/v1/search',
+                        params=payload, headers=headers).json()
+    return resp
+
+
+def get_genres(token):
+    """Returns a list of Spotify music genres"""
+
+    headers = {'Authorization': 'Bearer ' + token}
+    resp = requests.get(
+        'https://api.spotify.com/v1/recommendations/available-genre-seeds', headers=headers).json()
+    genres = [(genre, genre.capitalize()) for genre in resp['genres']]
+    genres[0] = ('', 'Select Genre')
+    return genres
+
+
+def get_keys():
+    """Returns a list keys"""
+
+    keys = [('', 'Select a Song Key'), ('1', 'C'), ('2', 'C♯/D♭'), ('3', 'D'), ('4', 'D♯/E♭'), ('5', 'E'),
+            ('6', 'F'), ('7', 'F♯/G♭'), ('8', 'G'), ('9', 'G♯/A♭'), ('10', 'A'), ('11', 'A♯/B♭'), ('12', 'B')]
+    return keys
+
+
+def get_modes():
+    """Returns a list modes (major or minor key)"""
+
+    keys = [('', 'Select Major or Minor Key'), ('0', 'Minor'), ('1', 'Major')]
+    return keys
+
+
+def create_playlist(user_id, token):
+    """Creates an empty Spotify playlist for the logged in Spotify account"""
+
+    headers = {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json'
+    }
+    data = '{"name": "Keymix Playlist","public":false}'
+    resp = requests.post(
+        f'https://api.spotify.com/v1/users/{user_id}/playlists', headers=headers, data=data).json()
+    return resp['id']
+
+
+def add_songs_to_playlist(playlist_id, track_list, token):
+    """Adds tracks to playlist for the logged in Spotify Account"""
+
+    headers = {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json'
+    }
+    params = {'uris': track_list}
+    resp = requests.post(
+        f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks', headers=headers, params=params).json()
+    return resp
