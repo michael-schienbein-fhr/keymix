bf092d09b705204303072c40ee9c800f250928f8
Michael Schiebein <moomba0@gmail.com>
2021-02-24 19:41:36 +0300

    add db

--- keymix/__init__.py
+++ keymix/__init__.py
@@ -7,14 +7,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # testing:
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///keymix'
-app.config['SECRET_KEY'] = 'secret_key'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///keymix'
+# app.config['SECRET_KEY'] = 'secret_key'
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 # debug = DebugToolbarExtension(app)
 # production:
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-#     'DATABASE_URL', 'postgres:///flask-heroku')
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'akey')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'postgres:///flask-heroku')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'akey')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
