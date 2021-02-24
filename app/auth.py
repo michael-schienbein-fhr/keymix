33e3b45fe1733a8426b28d0e5f70ff3af391112c
Michael Schiebein <moomba0@gmail.com>
2021-02-24 14:22:23 +0300

    add build files

---
+++ app/auth.py
@@ -0,0 +1,9 @@
+import base64
+
+client_id = 'a598514bd4fa4304b7ca63c5046fca6a'
+client_secret = 'ce25bcaa33434f6cae6e39d44ef48446'
+redirect_uri_encoded = 'http%3A%2F%2F127.0.0.1%3A5000%2Fuser_auth'
+redirect_uri = "http://127.0.0.1:5000/user_auth"
+secret_key = "4$2#%%3se@#$cret"
+encodedData = base64.b64encode(
+    bytes(f"{client_id}:{client_secret}", "ISO-8859-1")).decode("ascii")
