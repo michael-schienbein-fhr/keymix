a773f443b44ec50171272ef502cdd804e53541b3
Michael Schiebein <moomba0@gmail.com>
2021-02-24 20:06:16 +0300

    update auth

--- keymix/auth.py
+++ keymix/auth.py
@@ -2,8 +2,8 @@ import base64
 
 client_id = 'a598514bd4fa4304b7ca63c5046fca6a'
 client_secret = 'ce25bcaa33434f6cae6e39d44ef48446'
-redirect_uri_encoded = 'http%3A%2F%2F127.0.0.1%3A5000%2Fuser_auth'
-redirect_uri = "http://127.0.0.1:5000/user_auth"
+redirect_uri_encoded = 'https%3A%2F%2Fkeymix.herokuapp.com%2Fuser_auth'
+redirect_uri = "https://keymix.herokuapp.com/user_auth"
 secret_key = "4$2#%%3se@#$cret"
 encodedData = base64.b64encode(
     bytes(f"{client_id}:{client_secret}", "ISO-8859-1")).decode("ascii")
