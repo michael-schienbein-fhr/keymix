446411baaa98dba372f00a176e541854ad29416e
Michael Schiebein <moomba0@gmail.com>
2021-02-25 16:44:57 +0300

    testing

--- keymix/__init__.py
+++ keymix/__init__.py
@@ -1,3 +1,5 @@
+from keymix import routes, forms, models, auth, util
+from keymix.auth import secret_key
 import os
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
@@ -5,17 +7,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-### testing:
-from keymix.auth import secret_key
+# testing:
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///keymix'
 # app.config['SECRET_KEY'] = 'secret_key'
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-# debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
-### production:
+# production:
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL', 'postgres:///flask-heroku')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'akey')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', secret_key)
 
 ###
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -30,5 +31,4 @@ def connect_db(app):
 
 
 connect_db(app)
-from keymix import routes, forms, models, auth, util
 db.create_all()
